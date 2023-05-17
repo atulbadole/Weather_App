7d70ed9d46904d5ffb8485b21e144521f5e0898d
atulbadole <atul.cse19@chitkarauniversity.edu.in>
2023-05-17 15:38:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import styled from "styled-components";
 import './App.css';
+import Axios from "axios";
+import { useState } from "react";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherinfoComponent"; 
+const API_KEY="fe4feefa8543e06d4f3c66d92c61b69c"
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 7px 16px 0px #555;
+  background: white;
+  font-family: Montserrat;
+`;
 
-function App() {
+  const AppLabel = styled.span`
+ color:black;
+ font-size: 18px;
+ font-weight:bold;
+
+  `;
+  
+  function App() {
+    const [city, updateCity] = useState();
+    const [weather, updateWeather] = useState();
+    const fetchWeather = async (e) => {
+      e.preventDefault();
+      const response = await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+      );
+      updateWeather(response.data);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+    <Container>
+      <AppLabel>
+      React Weather App</AppLabel>
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+     
+    </Container>
   );
 }
 
---
+++ src/modules/CityComponent.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+const WeatherLogo=styled.img`
+width:140px;
+height:140px;
+margin:40px auto;
+`
+const ChooseCityLabel=styled.span`
+color:black;
+font-size:18px;
+font-weight;bold;
+margin:10px auto;
+`
+const SearchBox=styled.form`
+display:flex;
+flex-direction:row;
+border:black solid 1px;
+border-radius:2px;
+color:black;
+font-size:18px;
+font-weight:bold;
+margin:10px auto;
+& input{
+    padding:10px;
+    font-size:14px;
+    border:none;
+    font-weight: bold;
+}
+
+& button{
+    padding:10px;
+    font-size:14px;
+    border:none;
+    font-weight: bold;
+    color:white;
+    background-color:black;
+    outline:none;
+    cursor:pointer;
+
+}
+`;
+const CityComponent= (props) =>{
+    const {updateCity,fetchWeather}=props;
+    return (
+        <>
+        <WeatherLogo src="/icon/perfect-day.svg"></WeatherLogo>
+        <ChooseCityLabel>Find Weather of your City</ChooseCityLabel>
+        <SearchBox onSubmit={fetchWeather}>
+            <input placeholder="City" onChange={(e)=>updateCity(e.target.value)}/>
+            <button type="submit">Search</button>
+        </SearchBox>
+        </>
+    );
+};
+export default CityComponent;
